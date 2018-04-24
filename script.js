195f98958f8a767c44d54cb2a60cf2df7577d559
sblomk <sebbeblomkvist@gmail.com>
2018-04-24 14:52:35 +0200

    2

--- script.js
+++ script.js
@@ -1,17 +1,3 @@
-window.fn = {};
-
-window.fn.open = function() {
-  var menu = document.getElementById('menu');
-  menu.open();
-};
-
-window.fn.load = function(page) {
-  var content = document.getElementById('content');
-  var menu = document.getElementById('menu');
-  content.load(page)
-    .then(menu.close.bind(menu));
-};
-
 document.addEventListener('prechange', function(event) {
   document.querySelector('ons-toolbar .center')
     .innerHTML = event.tabItem.getAttribute('label');
