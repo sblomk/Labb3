fdf3733d276bbc0226a64595b36464f14e1eb764
sblomk <sebbeblomkvist@gmail.com>
2018-04-25 11:17:17 +0200

    6

--- script.js
+++ script.js
@@ -21,3 +21,22 @@ var hideDialog = function(id) {
     .getElementById(id)
     .hide();
 };
+
+var showTemplateDialogShare = function() {
+  var dialog = document.getElementById('share');
+
+  if (dialog) {
+    dialog.show();
+  } else {
+    ons.createElement('share.html', { append: true })
+      .then(function(dialog) {
+        dialog.show();
+      });
+  }
+};
+
+var hideDialogShare = function(id) {
+  document
+    .getElementById(id)
+    .hide();
+};
